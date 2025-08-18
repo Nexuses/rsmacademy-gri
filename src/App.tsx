d6777a268681b596ed3f07ffcfc0f0f604863884
Ekansh <efuforofficial@gmail.com>
2025-08-18 12:38:36 +0530

    trainer things

--- src/App.tsx
+++ src/App.tsx
@@ -36,7 +36,7 @@ function App() {
         <LearningApproach />
         <CourseOverview />
         <TrainerProfile />
-        <ChiefGuest />
+        {/* <ChiefGuest /> */}
         {/* <CourseAgenda /> */}
         {/* <Testimonials /> */}
         <RegistrationSection />
